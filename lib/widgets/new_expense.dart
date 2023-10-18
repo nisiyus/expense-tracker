0729fe9b324f8a82fc1df7a6f7c25821c69b20da
Dion <riadidion@gmail.com>
2023-10-18 08:43:52 +0700

    add function for adding expense

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() => _NewExpense();
@@ -52,6 +53,11 @@ class _NewExpense extends State<NewExpense> {
               )));
       return;
     }
+    widget.onAddExpense(Expense(
+        title: _titleController.text,
+        amount: enteredAmount!,
+        date: _selectedDate!,
+        category: _selectedCategory));
   }
 
   @override
